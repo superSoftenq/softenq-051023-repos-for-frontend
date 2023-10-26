014b3a804f9477d7b49a745d238064ebd8c23a8d
Dmitry_Moskvin <moskvin_03@mail.com>
2023-10-26 12:17:23 +0600

    заполнен файл для логина, стили для страницы регистрации, добавлены импорты в main.jsx, добавлены компоненты в main.jsx

---
+++ src/components/auth/signin.jsx
@@ -0,0 +1,74 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+  } from "react-router-dom";
+
+let count = 0;
+const max_count = 3;
+function SignIn() {
+  return (
+    <>
+        <h3>signin</h3>
+      <div className="form">
+        <form onSubmit={async (event) => {
+            event.preventDefault();
+            if(count >= max_count){
+                return;
+            }
+            let form = document.getElementById('signin');
+            let dataJson = {
+                username : form.querySelector('input[name="username"]').value,
+                password : form.querySelector('input[name="password"]').value
+            }
+            console.log(dataJson);
+            let response = await fetch("api/auth/signin", {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(dataJson)
+                })
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    if(data.accessToken != null){
+                        window.location.assign('/login_succeed');
+                    }
+                    else {
+                        count++;
+                        if(max_count - count > 0){
+                            all.innerHTML = "Invalid password or username. Attempts remained: " + (max_count - count);
+                        }
+                        else{
+                            all.innerHTML = "Too many unsuccessful attempts. Please try again later ";
+                        }
+                        
+                    }
+                });
+        }} id = "signin" method = "POST">
+            <input className = "form-field" name='username' type="text" placeholder='username'/>
+            <input className = "form-field" name='password' type="text" placeholder='password'/>
+            <input type="submit" value = "Login"/>
+        </form>
+      </div>
+    <div  id="all">
+    </div>
+    <div id="user">
+
+    </div>
+    <div id="mod">
+
+    </div>
+    <div id="admin">
+
+    </div>
+    
+    </>
+  )
+}
+
+export default SignIn
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './index.css'
 import SignIn from './components/auth/signin.jsx'
 import SignUp from './components/auth/signup.jsx'
 import LoginSucceed from './components/auth/login_succeed.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
