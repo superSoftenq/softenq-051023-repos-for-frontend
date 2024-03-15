2e14b50d2a5839ba43a26ed78f2c0041fedb0654
Dmitry_Moskvin <moskvin_03@mail.com>
2024-03-15 11:47:57 +0600

    отображение постов не работает, данные приходят, но функции выполняются в неправильном порядке, неизвестно почему..

--- src/components/pages/newPosts/pageWithNewsPosts.jsx
+++ src/components/pages/newPosts/pageWithNewsPosts.jsx
@@ -2,85 +2,101 @@ import Post from "./post";
 import './pageWithNewsPosts.css'
 import { NavLink, json } from "react-router-dom";
 import React from "react";
+let bigdata = {
 
-const PageWithNewsPosts= (props) => {
-    
-    let listPostsFromComp = props.DateForPost.posts.map
-    ( p => <Post 
-        id = {p.id}
-        avtorPosta = {p.avtorPosta}
-        timeByPost = {p.timeByPost}
-        message = {p.message} 
-        likecounter = {p.likecounter} 
-        repostCounter = {p.repostCounter}/>)
-
-    let dataForPost = 
-    {
-    ownerId: "568",
-    photoId: "7799"
 }
-     let createPost = () => {
-      fetch('/api/post/create', {
-        method: 'POST',
-        headers: { 
-           'Content-Type': 'application/json' // Устанавливаем заголовок Content-Type для указания типа данных 
-         }, 
-        body: JSON.stringify(dataForPost)}).then(response => { 
-           if (!response.ok) { 
-             throw new Error('Ошибка сети или сервера'); 
-           } 
-           return response.json(); // Парсим ответ сервера в формате JSON 
-         }) 
-         .then(data => { 
-           console.log(data); // Обрабатываем полученные данные 
-         }) 
-         .catch(error => { 
-           console.error(error); // Обрабатываем ошибки 
-         });
-     }
-     let dataForGetPost = {
-      startingPoint : 0,
-      postsCount: 9999,
-      flags: 0
-     }
-     let getPost = () => {
-      fetch('/api/feed', {
-        method: 'POST',
-        headers: { 
-           'Content-Type': 'application/json' // Устанавливаем заголовок Content-Type для указания типа данных 
-         }, 
-        body: JSON.stringify(dataForGetPost)}).then(response => { 
-           if (!response.ok) { 
-             throw new Error('Ошибка сети или сервера'); 
-           } 
-           return response.json(); // Парсим ответ сервера в формате JSON 
-         }) 
-         .then(data => { 
-           console.log(data); // Обрабатываем полученные данные 
-         }) 
-         .catch(error => { 
-           console.error(error); // Обрабатываем ошибки 
-         });
 
-         
-     }
-     
-    return(
+let dataForGetPost = {
+  startingPoint: 0,
+  postsCount: 9999,
 
-        
-        <div className="myPosts">
-                <div >
-                    <NavLink className="butForHome" to = '/myprofile'> My Profile</NavLink>
-                    </div>
-                <div>
-                    <div><textarea ></textarea></div>
-                    <div><button onClick={createPost}> add new Post</button></div>
-                    <div><button onClick={getPost}>view all post</button></div>
-                </div>
-                {listPostsFromComp}
-                
-        </div>
-    )
+  flags: 0
+}
+
+let dataForPost =
+{
+  ownerId: 568,
+  photoId: 7799,
+  comment:'hello this is my first post from dataBase'
+}
+
+let createPost = () => {
+  fetch('/api/post/create', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json' // Устанавливаем заголовок Content-Type для указания типа данных 
+    },
+    body: JSON.stringify(dataForPost)
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Ошибка сети или сервера');
+    }
+    return response.json(); // Парсим ответ сервера в формате JSON 
+  })
+    .then(data => {
+      console.log(data); // Обрабатываем полученные данные 
+    })
+    .catch(error => {
+      console.error(error); // Обрабатываем ошибки 
+    });
+}
+let getPost = () => {
+  fetch('/api/feed', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json' // Устанавливаем заголовок Content-Type для указания типа данных 
+    },
+    body: JSON.stringify(dataForGetPost)
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Ошибка сети или сервера');
+    }
+    return response.json(); // Парсим ответ сервера в формате JSON 
+  })
+    .then(data => {
+      console.log('data = ', data); // Обрабатываем полученные данные
+      bigdata = data 
+      
+    })
+    .catch(error => {
+      console.error(error); // Обрабатываем ошибки 
+    });
+
+
+}
+
+const PageWithNewsPosts = (props) => {
+
+  let listPostsFromComp = props.DateForPost.posts.map
+    (p => <Post
+      id={p.id}
+      avtorPosta={p.avtorPosta}
+      timeByPost={p.timeByPost}
+      message={p.message}
+      likecounter={p.likecounter}
+      repostCounter={p.repostCounter} />)
+
+  return (
+
+
+    <div className="myPosts">
+      <div >
+        <NavLink className="butForHome" to='/myprofile'> My Profile</NavLink>
+      </div>
+      <div>
+        <div><textarea ></textarea></div>
+        <div><button onClick={createPost}> add new Post</button></div>
+        <div><button>view all post</button></div>
+      </div>
+      <div>
+      {getPost()}
+        {console.log('bigdata = ',bigdata)}
+      </div>
+
+      {listPostsFromComp}
+
+    </div>
+  )
 }
 
 export default PageWithNewsPosts;
