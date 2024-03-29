9dd34e47f8274e29ffaf6f2ab42c8c3acb91e7fd
Dmitry_Moskvin <moskvin_03@mail.com>
2024-03-29 22:27:31 +0600

    исправил ссылку для отображения аватарки

--- src/components/pages/myProfile.jsx
+++ src/components/pages/myProfile.jsx
@@ -25,8 +25,11 @@ function MyProfile(props){
 
 
 
-    const googleLink = "https://drive.google.com/u/0/uc?id=";
-    const urlRight = "&export=download"
+    //const googleLink = "https://drive.google.com/u/0/uc?id=";
+    const googleLink = "https://drive.google.com/thumbnail?id="
+
+    //const urlRight = "&export=download"
+    const urlRight = "&sz=w1000"
     const params = useParams();
     // const link = async () => fetch(`/api/user/${user.id}/avatar`)
     const link = googleLink + user.profilePicture + urlRight
