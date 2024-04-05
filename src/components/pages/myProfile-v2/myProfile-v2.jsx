eec239899b135a5b25db4ce06b80c4465e0eac92
Dmitry_Moskvin <moskvin_03@mail.com>
2024-04-05 15:00:01 +0600

    успешное начало исправление верстки страницы пользователя, успешное получение всех данных и отрисовка аватарки и галереи.

--- src/components/pages/myProfile-v2/myProfile-v2.jsx
+++ src/components/pages/myProfile-v2/myProfile-v2.jsx
@@ -1,10 +1,26 @@
 import { useState, useEffect } from "react";
 import { verifyUser } from "../../includes/verifyUser.js";
 import * as Cookie from "../../includes/cookie.js"
+import { renderMyGallery } from "../userPageIncludes.jsx";
+import { renderAvatar } from "../userPageIncludes.jsx";
 
 const MyProfileV2 = (props) => {
+    const googleLink = "https://drive.google.com/thumbnail?id="
+    const urlRight = "&sz=w1000"
 
     const [id, setId] = useState([])
+
+    useEffect(() => {
+        getId()
+
+
+    }, []);
+
+
+    useEffect(() => {
+        getUserData()
+    }, [id])
+
 
     let token = Cookie.getCookie("token")
 
@@ -15,23 +31,69 @@ const MyProfileV2 = (props) => {
 
 
     const [user, setUser] = useState([])
+    const [statusCode, setStatusCode] = useState([])
+    const [photos, setPhotos] = useState([])
 
     const getUserData = () => {
         console.log("id = ", id)
-        
+
+        fetch("/api/user/" + id)
+            .then((response) => {
+                setStatusCode(response.status)
+                if (response.status == 200) {
+                }
+
+                return response.json();
+            })
+            .then((data) => {
+                console.log('user data = ', data)
+                setUser(data)
+                if (rs == -1) {
+                    setAuthorized(-1)
+                } else if (rs != data.id) {
+                    setAuthorized(false)
+                } else {
+                    setAuthorized(true)
+                }
+            });
+        const objForGetPhoto = {
+            flags: -1
+        }
+        fetch('/api/user/'+ id + '/photos', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json' // Устанавливаем заголовок Content-Type для указания типа данных 
+            },
+            body: JSON.stringify(objForGetPhoto)
+          }).then(response => {
+            if (!response.ok) {
+              throw new Error('Ошибка сети или сервера');
+            }
+            return response.json(); // Парсим ответ сервера в формате JSON 
+          })
+            .then(data => {
+              console.log('data user-s photo', data); //это почему-то выполняется бесконечное число раз
+              setPhotos(data)
+            })
+            .catch(error => {
+              console.error(error); // Обрабатываем ошибки 
+            });
+
+
+
+
     }
+    console.log("global user data = ", user)
 
 
 
-    useEffect(() => {
-        getId()
-        getUserData() //эта функция при первой загрузке не выполняется
 
-      }, []);
     return (
         <div>
             <div>{id}</div>
-            hello this is page MyProfile version-2
+           <div>hello this is page MyProfile version-2</div>
+            <div>{user.profilePicture !== undefined && renderAvatar(googleLink + user.profilePicture + urlRight)}</div>
+            {photos.length != 0 && renderMyGallery(photos)}
         </div>
     )
 }
