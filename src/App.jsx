fbfd41e99d061f7676df80ebdf10cdd6dba2a17d
moskvin_dmitry <moskvin_03@mail.ru>
2024-04-24 17:00:32 +0600

    добавил модалку через createPortal все работает авторизация не ломается

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Modal from './components/pages/modalPortal/Modal'
 
 function App() {
-  
+
+ 
+  const [open, setOpen] = useState(false);
+
   const [count, setCount] = useState(0)
   let response = fetch('/api/fr')
-  .then(response => response.text());
+    .then(response => response.text());
   console.log(response);
   return (
     <>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={() => setOpen(true)}> Открыть или закрыть</button>
+        <Modal 
+        children = '123'
+        open = {open}
+        onClose = {()=> setOpen(false)}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/pages/modalPortal/Modal.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useMemo } from "react"
+import { createPortal } from "react-dom"
+import style from './Modal.module.css'
+
+const modalRootElement = document.querySelector("#modal")
+const Modal = (props) => {
+    console.log('props = ', props)
+
+    
+    const open = props.open
+    const onClose = props.onClose
+    const element = useMemo(() => document.createElement("div"), []);
+
+
+
+    useEffect(() => {
+        if (open) {
+            modalRootElement.appendChild(element);
+
+            return () => {
+                modalRootElement.removeChild(element)
+            };
+        }
+
+    })
+
+    if (open) {
+        return createPortal(
+            <div className={style.modal_background} onClick={onClose}>
+                <div className={style.modal_card}>
+                    hello
+                </div>
+            </div>, element)
+
+    }
+    return null;
+
+}
+export default Modal;
